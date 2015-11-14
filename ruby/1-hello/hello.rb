9c7e9a1070f5658a58121ec69901cadef063f8fa
Robie <robie1373@gmail.com>
2015-11-14 15:10:30 +0000

    finished ruby solutions with challenges

--- ruby/1-hello/hello.rb
+++ ruby/1-hello/hello.rb
@@ -1,4 +1,5 @@
 class Hello
+  attr_accessor :name
   def initialize(input: $stdin, output: $stdout)
     @input = input
     @output = output
@@ -12,14 +13,49 @@ class Hello
   def say_hello
     @output.puts "Hello #{@name}. Nice to see you!"
   end
+end
 
-  def name?
-    @name
+class Hello2
+  def initialize(input: $stdin, output: $stdout)
+    @input = input
+    @output = output
+  end
+  
+  def ask_name
+    @output.print("What is your name? ")
+  end
+
+  def say_hello
+    @output.puts("Hello #{@input.gets.strip!}. Nice to see you!")
+  end
+end
+
+class Hello3
+  attr_accessor :name
+  def initialize(input: $stdin, output: $stdout)
+    @input = input
+    @output = output
+  end
+
+  def ask_name
+    @output.print("What is your name? ")
+    @name = @input.gets.strip!
+  end
+
+  def say_hello
+    case @name
+      when "Robie"
+        @output.puts "Hello #{@name}. Nice to see you!"
+      when "Toby"
+        @output.puts "Hello #{@name}. Are you going to chase the squirrel?"
+      else
+        @output.puts "Howdy #{@name}"
+    end
   end
 end
 
 if __FILE__ == $0
-  hello = Hello.new
+  hello = Hello3.new
   hello.ask_name
   hello.say_hello
 end
--- ruby/1-hello/hello_test.rb
+++ ruby/1-hello/hello_test.rb
@@ -7,7 +7,7 @@ class TestHello < Minitest::Unit::TestCase
   def setup
     @outputio = StringIO.new
     @inputio = StringIO.new("Robie\n")
-    @hello = Hello.new(input: @inputio, output: @outputio) 
+    @hello = Hello2.new(input: @inputio, output: @outputio) 
     @hello.ask_name
   end
 
@@ -15,20 +15,61 @@ class TestHello < Minitest::Unit::TestCase
     res = @outputio.string
     expected = "What is your name? "
 
-    assert res == expected, "Expected #{res} to match #{expected}"
-  end
-
-  def test_name?
-    assert_equal @hello.name?, "Robie"
+    assert res == expected, "Expected #{res}\n to match #{expected}"
   end
 
   def test_say_hello
     @hello.say_hello
-    puts @outputio.string 
     res = @outputio.string.strip!
     expected = "What is your name? Hello Robie. Nice to see you!"
 
     assert res == expected, 
-      "Expected #{res} to match #{expected}"
+      "Expected #{res}\n to match #{expected}"
+  end
+end
+
+
+class TestHello3 < Minitest::Unit::TestCase
+  def setup
+  end
+
+  def test_ask_name
+    outputio = StringIO.new
+    inputio = StringIO.new("Robie\n")
+    hello = Hello3.new(input: inputio, output: outputio) 
+    hello.ask_name
+
+    res = outputio.string
+    expected = "What is your name? "
+
+    assert res == expected, "Expected #{res}\n to match #{expected}"
+  end
+
+  def test_say_hello_robie
+    outputio = StringIO.new
+    inputio = StringIO.new("Robie\n")
+    hello = Hello3.new(input: inputio, output: outputio) 
+    hello.ask_name
+
+    hello.say_hello
+    res = outputio.string.strip!
+    expected = "What is your name? Hello Robie. Nice to see you!"
+
+    assert res == expected, 
+      "Expected #{res}\n to match #{expected}"
+  end
+
+  def test_say_hello_toby
+    outputio = StringIO.new
+    inputio = StringIO.new("Toby\n")
+    hello = Hello3.new(input: inputio, output: outputio) 
+    hello.ask_name
+
+    hello.say_hello
+    res = outputio.string.strip!
+    expected = "What is your name? Hello Toby. Are you going to chase the squirrel?"
+
+    assert res == expected, 
+      "Expected #{res}\n to match #{expected}"
   end
 end
